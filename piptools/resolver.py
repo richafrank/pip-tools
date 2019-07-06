e5365f51ec76acc4066b9b067c38bcc4a4e57050
Richard Frank <rich@quantopian.com>
2019-07-06 14:27:35 -0400

    support for pinned url

--- piptools/resolver.py
+++ piptools/resolver.py
@@ -322,6 +322,16 @@ class Resolver(object):
         best_match.comes_from = ireq.comes_from
         return best_match
 
+    def _is_unpinned_url_req(self, ireq):
+        if is_url_requirement(ireq):
+            if is_pinned_requirement(ireq):
+                return False
+
+            self.repository.get_dependencies(ireq)
+            return not is_pinned_requirement(ireq)
+
+        return False
+
     def _iter_dependencies(self, ireq):
         """
         Given a pinned, url, or editable InstallRequirement, collects all the
@@ -331,10 +341,12 @@ class Resolver(object):
         Editable requirements will never be looked up, as they may have
         changed at any time.
         """
-        if ireq.editable or is_url_requirement(ireq):
+        if ireq.editable or self._is_unpinned_url_req(ireq):
             for dependency in self.repository.get_dependencies(ireq):
                 yield dependency
+
             return
+
         elif not is_pinned_requirement(ireq):
             raise TypeError(
                 "Expected pinned or editable requirement, got {}".format(ireq)
@@ -368,7 +380,10 @@ class Resolver(object):
             )
 
     def reverse_dependencies(self, ireqs):
-        non_editable = [
-            ireq for ireq in ireqs if not (ireq.editable or is_url_requirement(ireq))
+        pinned = [
+            ireq
+            for ireq in ireqs
+            if not (ireq.editable or self._is_unpinned_url_req(ireq))
         ]
-        return self.dependency_cache.reverse_dependencies(non_editable)
+
+        return self.dependency_cache.reverse_dependencies(pinned)
